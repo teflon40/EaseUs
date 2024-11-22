4c2fcae0215754e46e53b3017d551a60399dae3a
teflon40 <frimpong.bernard3220@gmail.com>
2024-11-22 13:03:58 +0000

    Neovim config switcher in progree

---
+++ nvim-switcher.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+
+# A function that helps to switch between various neovim configs
+#
+alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
+alias nvim-kick="NVIM_APPNAME=kickstart nvim"
+alias nvim-chad="NVIM_APPNAME=NvChad nvim"
+alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
+
+function nvims() {
+  # items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim")
+  items=$(find $HOME/.config -maxdepth 2 -name "init.lua" -type f -execdir sh -c 'pwd | xargs basename' \;)
+  selected=$(printf "%s\n" "${items[@]}" | FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS-} --preview-window 'right:border-left:50%:<40(right:border-left:50%:hidden)' --preview 'lsd -l -A --tree --depth=1 --color=always --blocks=size,name ~/.config/{} | head -200'" fzf)
+  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+  if [[ -z $config ]]; then
+    echo "Nothing selected"
+    return 0
+  elif [[ $config == "default" ]]; then
+    config=""
+  else
+    alias nv="NVIM_APPNAME=${NVIM_APPNAME:-${config}} nvim"
+  fi
+  NVIM_APPNAME=$config nvim $@
+}
+
+
